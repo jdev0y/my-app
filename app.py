6486ce0ce2fbef262f26dc913f8caaabe01cadb1
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-07-09 19:20:30 +0300

    comments added to explain code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 print("Welcome to Python") #this is a comment
 
+#this function is for adding 2 numbers
 def add_num (x, y):
     print("the total is", x+y)
 
-
+#these are the user inputs who ask the user for numbers to add
 ask_user = int(input("choose a number"))
 ask_user2 = int(input("choose another number"))
 
+#this is the function call
 add_num(ask_user, ask_user2)
