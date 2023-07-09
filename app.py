269de370433899234755bd00891ba6e1ab5d1898
AlJawharah AlQahtani <aljawharahalqahtani@AlJawharahs-MacBook-Pro.local>
2023-07-09 19:18:41 +0300

    add num function added

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("Welcome to Python") #this is a comment
+
+def add_num (x, y):
+    print("the total is", x+y)
+
+
+ask_user = int(input("choose a number"))
+ask_user2 = int(input("choose another number"))
+
+add_num(ask_user, ask_user2)
