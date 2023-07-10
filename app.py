938eb3eba83fb738bc0c462b4ef31c448fd19f9d
Jdev0x <aljawharah@local.host>
2023-07-10 18:44:20 +0300

    function body added

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 # TODO: format each element in the list(start_string the_list end_string)
 def format_the_list_elements(the_list, start_string, end_string):
-    pass
+    for i in the_list:
+        print(start_string, the_list[i], end_string)
+
+format_the_list_elements([2, 3, 4], "<!", "!>")
