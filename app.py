f4e9f572b703a66f87d37301c62445f5a85c76f9
Jdev0x <aljawharah@local.host>
2023-07-10 18:50:20 +0300

    TODO comment deleted from app.py
    I deleted the TODO comment that was in the first line, because it
    talked about what we should do in the function, which is formating
    each element and we already done that so I deleted. users @Jdev0x
    and @Jdev0y were the ones working on the function.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# TODO: format each element in the list(start_string the_list end_string)
 def format_the_list_elements(the_list, start_string, end_string):
     for element in the_list:
         print(start_string, element, end_string)
